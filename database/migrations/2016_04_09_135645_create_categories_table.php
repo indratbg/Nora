cb3c2c4d90fd8db7c229619965f52a89a313f4df
Indra Tobing <indra.tbg@gmail.com>
2016-04-09 14:21:16 +0700

    tambahan

---
+++ database/migrations/2016_04_09_135645_create_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->string('category_id1',10);
+            $table->string('category_id2',10);
+            $table->string('category_name',30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('categories');
+    }
+}
